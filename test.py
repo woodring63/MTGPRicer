f7122c028ac4f1759e28cc7019d48f5eb4801c6b
Woodring <ew051883@m1711825.northamerica.cerner.net>
2017-07-07 13:42:27 -0500

    didn't do much

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 from mtgsdk import Card
 import json
 import urllib.request
-req = urllib.request.Request("https://api.magicthegathering.io/v1/cards?name=humility", headers={'User-Agent': 
-	'Mozilla/5.0'})
-card = urllib.request.urlopen(req)
-data = json.load(card)
-print(data['cards'][len(data['cards'])-1]['text'])
+card = Card.where(name="garruk, primal hunter").all()
+print(card[0].text)
 
